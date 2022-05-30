8d12a0e90580815da1ca23aafbbcda14690321d5
estebanmilanesio <esmilanesio@gmail.com>
2022-05-30 17:29:31 -0300

    vuelta al estado original

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ const express = require('express');
 const path = require ('path');
 const app = express();
 
-const port_number = server.listen(process.env.PORT || 3000);
 
-app.listen(port_number);
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-/*app.listen(3000, ()=>{
+app.listen(3000, ()=>{
     console.log('Servidor funcionando');
-});*/
+});
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
